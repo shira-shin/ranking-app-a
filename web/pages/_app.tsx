14d278fd2b39b4e6ac755c134849773ed22ac939
shira-shin <shiratorien9837@gmail.com>
2025-06-07 17:14:21 +0900

    Configure default time zone for NextIntl

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -6,8 +6,11 @@ import '../styles.css';
 export default function MyApp({ Component, pageProps }: AppProps) {
   const { locale } = useRouter();
   const messages = require(`../messages/${locale}.json`);
+  // Set a global default time zone to avoid server/client mismatches
+  // See https://next-intl.dev/docs/configuration#time-zone
+  const timeZone = process.env.NEXT_PUBLIC_TIME_ZONE || 'UTC';
   return (
-    <NextIntlClientProvider locale={locale!} messages={messages}>
+    <NextIntlClientProvider locale={locale!} messages={messages} timeZone={timeZone}>
       <Component {...pageProps} />
     </NextIntlClientProvider>
   );
