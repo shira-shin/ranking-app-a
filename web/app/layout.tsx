2612fff72e0ed8968200d62926ff58735d383825
shira-shin <shiratorien9837@gmail.com>
2025-08-03 21:32:29 +0900

    feat: add SessionProvider wrapper for app router

---
+++ web/app/layout.tsx
@@ -0,0 +1,12 @@
+import '../globals.css';
+import SessionProviderWrapper from './providers/SessionProviderWrapper';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <body>
+        <SessionProviderWrapper>{children}</SessionProviderWrapper>
+      </body>
+    </html>
+  );
+}
