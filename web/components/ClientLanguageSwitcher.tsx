d9ce2282a64bed3ff5964b074be36334c5747d15
shira-shin <shiratorien9837@gmail.com>
2025-08-13 22:02:29 +0900

    fix: mark language switcher as client component

---
+++ web/components/ClientLanguageSwitcher.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import LanguageSwitcher from "./LanguageSwitcher";
+
+export default function ClientLanguageSwitcher(
+  props: React.ComponentProps<typeof LanguageSwitcher>
+) {
+  return <LanguageSwitcher {...props} />;
+}
