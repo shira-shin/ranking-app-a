16aacab306a97b44b531bd5f15dff77a38c63f2f
shira-shin <shiratorien9837@gmail.com>
2025-08-14 13:30:51 +0900

    chore: remove experimental turbo config

--- web/next.config.js
+++ web/next.config.js
@@ -26,27 +26,6 @@ const nextConfig = {
   },
   reactStrictMode: true,
   env,
-  experimental: {
-    turbo: {
-      // Mirror the webpack aliases in Turbopack so that dev builds also
-      // resolve to the local React installation. Without this, Windows
-      // environments that have a global copy of React can end up loading
-      // two different React instances and trigger "Invalid hook call"
-      // errors at runtime.
-      resolveAlias: {
-        react: path.resolve(__dirname, './node_modules/react'),
-        'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-        'react/jsx-runtime': path.resolve(
-          __dirname,
-          './node_modules/react/jsx-runtime.js'
-        ),
-        'react/jsx-dev-runtime': path.resolve(
-          __dirname,
-          './node_modules/react/jsx-dev-runtime.js'
-        ),
-      },
-    },
-  },
   webpack: (config) => {
     // Ensure all packages, including ones hoisted outside this project, use
     // the same React instance to avoid "Invalid hook call" errors.
